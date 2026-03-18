57818ae2289a8db67fe7d7699f95991a87a3682c
tomato716 <pjh203203@naver.com>
2026-03-18 21:38:11 +0900

    Initial commit

---
+++ src/main/java/com/example/codereminder/CodeReminderApplication.java
@@ -0,0 +1,13 @@
+package com.example.codereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codereminder/CodeReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codereminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
