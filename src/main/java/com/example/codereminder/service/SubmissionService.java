6d90460d7e14040a9765ab9195aee5c39c84f7b1
tomato716 <pjh203203@naver.com>
2026-03-19 10:52:04 +0900

    feat: SubmissionService 구현

---
+++ src/main/java/com/example/codereminder/service/SubmissionService.java
@@ -0,0 +1,26 @@
+package com.example.codereminder.service;
+
+import com.example.codereminder.domain.Submission;
+import com.example.codereminder.dto.SubmissionDto;
+import com.example.codereminder.repository.SubmissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SubmissionService {
+    private final SubmissionRepository submissionRepository;
+
+    public void save(SubmissionDto dto) {
+        //해당 문제가 db에 있는지 조회
+        //있다면 복습날짜인지 체크
+        //처음 풀었을 때 바로 맞추면 db에서 제거 후 return
+
+        //db에 없는데 처음 풀었을 때 바로 맞추면 return
+
+        //당일 틀린 이후 또 풀었으면 return
+
+        Submission submission = Submission.from(dto);
+        submissionRepository.save(submission);
+    }
+}
