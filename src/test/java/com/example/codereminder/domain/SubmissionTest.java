bd57546a08b7be8bc43d36aed2fa22b8a8b322ef
tomato716 <pjh203203@naver.com>
2026-03-20 20:54:32 +0900

    test(Submission): 테스트 코드 작성

---
+++ src/test/java/com/example/codereminder/domain/SubmissionTest.java
@@ -0,0 +1,51 @@
+package com.example.codereminder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SubmissionTest {
+
+    @DisplayName("복습 날짜가 1, 3, 7, 21에 해당한다면 성공한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 7, 21})
+    void isReviewDay_Success(int reviewDay) {
+        //given
+        LocalDate now = LocalDate.now();
+        long dateOfFirstFailure = now.atStartOfDay(ZoneId.of("Asia/Seoul"))
+                .toInstant()
+                .toEpochMilli();
+        Submission submission = Submission.of("1", "park", 10L, "틀렸습니다", dateOfFirstFailure, dateOfFirstFailure);
+        LocalDate dateToReview = now.plusDays(reviewDay);
+
+        //when
+        boolean result = submission.isReviewDay(dateToReview);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("복습 날짜가 1, 3, 7, 21에 해당하지 않다면 실패한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {2, 5, 10, 22})
+    void isReviewDay_Failure(int reviewDay) {
+        //given
+        LocalDate now = LocalDate.now();
+        long dateOfFirstFailure = now.atStartOfDay(ZoneId.of("Asia/Seoul"))
+                .toInstant()
+                .toEpochMilli();
+        Submission submission = Submission.of("1", "park", 10L, "틀렸습니다", dateOfFirstFailure, dateOfFirstFailure);
+        LocalDate dateToReview = now.plusDays(reviewDay);
+
+        //when
+        boolean result = submission.isReviewDay(dateToReview);
+
+        //then
+        assertThat(result).isFalse();
+    }
+}
