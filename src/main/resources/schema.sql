83d41bfa84f233b65b9f85f8f966f6e885cbbf82
tomato716 <pjh203203@naver.com>
2026-03-20 20:58:22 +0900

    chore: ddl 유저아이디, 문제아이디 not null로 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,8 @@ drop table if exists submission cascade;
 
 create table submission(
     id varchar(36) primary key ,
-    user_id varchar(255),
-    problem_id bigint,
+    user_id varchar(255) not null ,
+    problem_id bigint not null ,
     result_text varchar(255),
     timestamp bigint,
     last_attempt_date bigint
