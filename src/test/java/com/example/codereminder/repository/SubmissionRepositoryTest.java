c93b8b7387998fb6636f987d01491ba514ddcacd
tomato716 <pjh203203@naver.com>
2026-03-21 17:26:52 +0900

    refactor(SubmissionRepository): 안쓰는 import 제거

--- src/test/java/com/example/codereminder/repository/SubmissionRepositoryTest.java
+++ src/test/java/com/example/codereminder/repository/SubmissionRepositoryTest.java
@@ -14,7 +14,6 @@ import java.time.ZoneId;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 @JdbcTest
