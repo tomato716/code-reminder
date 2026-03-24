ab632f1e542e3e942bbeb9d6944a107be46b3d67
tomato716 <pjh203203@naver.com>
2026-03-24 22:58:58 +0900

    chore: 개발 로그 info -> debug로 변경

--- src/main/java/com/example/codereminder/controller/ReviewItemController.java
+++ src/main/java/com/example/codereminder/controller/ReviewItemController.java
@@ -22,7 +22,7 @@ public class ReviewItemController {
 
     @PostMapping
     public String saveSubmission(@Validated @RequestBody ReviewItemDto dto){
-        log.info("도착 데이터={}", dto);
+        log.debug("도착 데이터={}", dto);
         reviewItemService.save(dto);
 
         return "ok";
@@ -31,7 +31,7 @@ public class ReviewItemController {
     @GetMapping("/{userName}/today-count")
     public ResponseEntity<Map<String, Integer>> getTodayReviewCount(@PathVariable String userName) {
         int count = reviewItemService.getReviewItems(userName, LocalDate.now()).size();
-        log.info("{}님의 오늘 복습할 문제 개수: {}", userName, count);
+        log.debug("{}님의 오늘 복습할 문제 개수: {}", userName, count);
 
         return ResponseEntity.ok(Map.of("today-count", count));
     }
